bdddf35bf1a33fc500ef449926fbbf7c4b2d696f
iosdev.sw <iosdev.sw@gmail.com>
2023-06-01 14:13:53 +0900

    Fix: Fix memory leak

--- StopWatch/Custom/RoundTextField.swift
+++ StopWatch/Custom/RoundTextField.swift
@@ -29,7 +29,7 @@ final class RoundTextField: UIView {
         
     }
     
-    var delegate: UITextFieldDelegate? {
+    weak var delegate: UITextFieldDelegate? {
         didSet {
             self.textfield.delegate = delegate
         }
